64dbca2c920ad0560b995691e3391984af185d10
Ashish Mudaliar <ashish.mudaliar16@gmail.com>
2018-02-18 12:22:59 +0100

    Add database models file for the project
    The models file contains the schema for the database to be used for this project. 3 tables are declared in the form of classes for the database.
    - The user_data table contains the user data for the employees. This includes their username, email and password for their account. The password is stored in an encrypted form.
    - The tickets table contains all the information about a ticket.  It includes the creation data of ticket, subject message and status of ticket.
    - The comments table contains all the comments added to a ticket by employees. The comments table is connected to the user_dat and tickets tables through foreign keys.
    
    Scripts to be added to add the schema from the models file to the database

---
+++ backend/ticket_manager/models.py
@@ -0,0 +1,48 @@
+import bcrypt
+import enum
+from ticket_manager.database import db
+
+class StatusEnum(enum.Enum):
+    Open = 1
+    In_Progress = 2
+    Completed = 3
+    Rejected = 4
+
+class UrgencyEnum(enum.Enum):
+    Low = 1
+    Mid = 2
+    High = 3
+
+class UserData(db.Model):
+    __tablename__ = "user_data"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, unique=True)
+    password_hash = db.Column(db.String)
+    email = db.Column(db.String)
+
+    def __init__(self, username, password,email):
+        self.username = username
+        self.password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+        self.email = email
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
+
+
+class Ticket(db.Model):
+    __tablename__ = "ticket"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String)
+    subject = db.Column(db.String)
+    status = db.Column(db.Enum(StatusEnum),nullable=False)
+    message = db.Column(db.Text)
+    urgency = db.Column(db.Enum(UrgencyEnum))
+    creation_date = db.Column(db.DateTime)
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user_data.id'))
+    text = db.Column(db.Text)
+    comment_date = db.Column(db.DateTime)
