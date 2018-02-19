6b50d9725be050561308805d49a63a59f3f68311
Ashish Mudaliar <ashish.mudaliar16@gmail.com>
2018-02-19 12:09:56 +0100

    Add input text vallidation for Ticket submission
    Validate the form data to check if any entry is not empty. Return 400 Error if found  to be empty. Earlier there was no validation for the input. Hence, tickets with empty email, subject or message could be created.

--- backend/ticket_manager/ticket_submission_api_handler.py
+++ backend/ticket_manager/ticket_submission_api_handler.py
@@ -8,9 +8,27 @@ from ticket_manager.database import db
 def create_api(app):
     ticket_submission_api = Blueprint('ticket_submission_api', __name__)
 
+    def validate_text(text):
+        if text == '':
+            return False
+        return True
+
+    def validate(data):
+        if not validate_text(data['subject']):
+            return False
+        if not validate_text(data['message']):
+            return False
+        if not validate_text(data['urgency']):
+            return False
+        if not validate_text(data['email']):
+            return False
+        return True
+
     @ticket_submission_api.route('/submit-ticket',methods=['POST'])
     def submit_ticket():
         try:
+            if not validate(request.form):
+                return "Please fill all the columns", 400, {"Access-Control-Allow-Credentials": "true"}
             email = request.form['email']
             subject = request.form['subject']
             message = request.form['message']
